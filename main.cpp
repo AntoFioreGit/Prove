9dd5059c02ee54beb879985b6062f7c3ce2c3fe0
afiorentino <antonio.fiorentino@contractro.datalogic.com>
2024-12-11 11:30:19 +0100

    terzo commit  su branch a

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ int main () {
 
     std::cout << "Una nuova riga di codice\n";
     std::cout << "Una nuova riga di codice sul branch A\n";
+    std::cout << "Una seconda riga nuova riga di codice sul branch A\n";
+
 
 
     return -1;
