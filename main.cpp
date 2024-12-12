580a57b8fa5c957f32dbd8d5a298ed7e35a1501f
afiorentino <antofiore1970@gmail.com>
2024-12-12 15:04:52 +0100

    genera un conflitto

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main () {
 
     std::cout << "Una nuova riga di codice\n";
     std::cout << "Una nuova riga di codice sul branch A\n";
-    std::cout << "Una seconda riga nuova riga di codice sul branch A\n";
+    std::cout << "Genera un conflitto\n";
     return -1;
 
 }
