0b7563f71de06f59109943761622286483daa464
AntoFioreGit <antofiore1970@gmail.com>
2025-05-21 10:18:03 +0200

    partenza da  situazione pulita

--- main.cpp
+++ main.cpp
@@ -3,29 +3,6 @@
 int main () {
     std::cout << "Hello word\n";
 
-    std::cout << "Una nuova riga di codice\n";
-    std::cout << "Una nuova riga di codice sul branch A\n";
-    std::cout << "Una seconda riga nuova riga di codice sul branch A\n";
-     std::cout << "Una terza riga nuova riga di codice sul branch A\n";
-      std::cout << "Una terza riga nuova riga di codice sul branch AAA\n";
-      std::cout << "Una terza riga nuova riga di codice sul branch A\n";
-      //jjjj
-      XXX;
-    //Remoto
-    //REMOTO
-    //GENERA_CONF
-
-
-
-//jjjjjj
-//ytytyttyttytytyuuy
-//Altra istruzione
-
-//Modifica 2
-//Modifica 3
-//Modifica 4
-    return -1;
-    //GENERA_CONFLITTO_RAMO_MASTER
-     //GENERA_CONFLITTO_RAMO_MASTER
+  
 
 }
